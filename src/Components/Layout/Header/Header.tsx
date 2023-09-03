dd3e320b021f6d8930f32ee6a29632ec20d09606
Ekremamass <Ekremamass@gmail.com>
2023-09-04 00:00:26 +0300

    reformatted header

--- src/Components/Layout/Header/Header.tsx
+++ src/Components/Layout/Header/Header.tsx
@@ -3,17 +3,16 @@ import AuthMenu from "../../Auth/AuthMenu/AuthMenu";
 import SelectLanguage from "../SelectLanguage/SelectLanguage";
 import "./Header.css";
 
-
 function Header(): JSX.Element {
   const { t } = useTranslation();
-  
+
   return (
     <div className="Header">
-    <h1>🎫{t('title')}</h1>
-    <div className="info">
-    <SelectLanguage/>
-      <AuthMenu />
-    </div>
+      <h1>🎫{t("title")}</h1>
+      <div className="info">
+        <SelectLanguage />
+        <AuthMenu />
+      </div>
     </div>
   );
 }
