618c7a16558a8b69182373e0b31c43511146e8a1
Ekremamass <Ekremamass@gmail.com>
2023-09-03 22:42:30 +0300

    url change

--- src/Services/UrlService.ts
+++ src/Services/UrlService.ts
@@ -1,7 +1,7 @@
 class UrlService {
-  private port = 8080;
-    private baseUrl = `http://localhost:${this.port}`;
-    // private baseUrl = `https://springcouponsystem-production.up.railway.app`;
+  // private port = 8080;
+  //   private baseUrl = `http://localhost:${this.port}`;
+    private baseUrl = `https://springcouponsystem-production.up.railway.app`;
     public admin = this.baseUrl + "/api/admin";
   public company = this.baseUrl + "/api/company";
   public customer = this.baseUrl + "/api/customer";
